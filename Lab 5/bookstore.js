a5ddaf07de07ccbae118dc3bea45964324f44dbb
radunphy <48693781+radunphy@users.noreply.github.com>
2022-10-16 17:30:40 -0400

    Added startup code

---
+++ Lab 5/bookstore.js
@@ -0,0 +1,16 @@
+/********************************************/
+//Global scope
+//Define book object here 
+
+
+
+
+/********************************************/
+function getDiscountedPrices()
+{
+
+}
+
+function getDiscountPercent()
+{
+}

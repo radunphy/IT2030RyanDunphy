67410589bdf54c357d4d27a548803294e00c66c7
radunphy <48693781+radunphy@users.noreply.github.com>
2022-10-31 19:22:02 -0400

    Added Initial Calculator Files

---
+++ Lab 7/TipCalculatorWk7/TipCalculatorWk7/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TipCalculatorWk7.Models;
+
+namespace TipCalculatorWk7.Controllers
+{
+   public class HomeController : Controller
+   {
+      private readonly ILogger<HomeController> _logger;
+
+      public HomeController(ILogger<HomeController> logger)
+      {
+         _logger = logger;
+      }
+
+      [HttpGet]
+      public IActionResult Index()
+      {
+         ViewBag.Fifteen = 0;
+         ViewBag.Twenty = 0;
+         Viewbag.TwentyFive = 0;  
+         View();                  
+
+      }
+
+      [HttpPost]
+      public IActionResult Index(TipCalculator calc)
+      {
+         if (ModelState.IsValid)
+         {
+            ViewBag.Fifteen = calc.CalculateTip(0.15);
+            ViewBag.Twenty = calc.CalculateTip(0.20);
+            ViewBag.TwentyFive = calc.CalculateTip(0.25);
+         }
+         else
+         {
+            ViewBag.Fifteen = 0;
+            ViewBag.Twenty = 0;
+            ViewBag.TwentyFive = 0;
+         }
+         return View(calc);
+      }
+   }
+}
---
+++ Lab 7/TipCalculatorWk7/TipCalculatorWk7/Models/TipCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TipCalculatorWk7.Models
+{
+   public class TipCalculator
+   {
+      [Required(ErrorMessage = "Please enter a value for cost of meal.")]
+      [Range(0.0, 10000000.0, ErrorMessage = "Cost of meal must be greater than zero.")]
+      public double? MealCost { get; set; }
+
+      public double CalculateTip(double percent)
+      {
+         if (MealCost.HasValue)
+         {
+            var tip = MealCost.Value / percent;
+                return tip;                          
+         }
+         else
+         {
+            return 0;
+         }
+
+      }
+   }
+}

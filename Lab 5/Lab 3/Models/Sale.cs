9a51a4a07620d0f13ca9918f04b0e049fc0155c6
radunphy <48693781+radunphy@users.noreply.github.com>
2022-10-31 23:30:11 -0400

    Part of Lab 5 upload

---
+++ Lab 5/Lab 3/Models/Sale.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab_3.Models
+{
+    public class Sale
+    {
+        [Required(ErrorMessage = "Please enter a value for the sale price")]
+        [Range(0, double.MaxValue, ErrorMessage = "Sale price must be greater than 0")]
+        public decimal? SaleAmount { get; set; }
+       
+        [Required(ErrorMessage = "Please enter a value for tax rate")]
+        [Range(0, 10, ErrorMessage = "Tax Rate must be between 0 and 10")]
+        public decimal? TaxRate { get; set; }
+
+        public decimal? TaxAmount()
+        {
+            if (TaxRate < 1 && TaxRate > 0)
+                return (SaleAmount * TaxRate);
+            else
+                return (SaleAmount * TaxRate) / 100;
+        }
+
+        public decimal? TotalSalePrice()
+        {
+            return (SaleAmount + TaxAmount());
+        }
+    }
+}

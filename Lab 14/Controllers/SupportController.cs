acef2a9c800b7e882afadfdf295b305682d0ad00
radunphy <48693781+radunphy@users.noreply.github.com>
2022-12-11 23:35:54 -0500

    Added Controllers

---
+++ Lab 14/Controllers/SupportController.cs
@@ -0,0 +1,21 @@
+﻿using Lab_3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_3.Controllers
+{
+    public class SupportController : Controller
+    {
+        [HttpGet]
+        public IActionResult Support()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Support(Customer customer)
+        {
+            var Customer = new Customer();
+            return View(customer);
+        }
+    }
+}
